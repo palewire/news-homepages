1771e129b89c800d216782b70416b11181159f00
palewire <b@palewi.re>
2022-07-23 03:48:15 -0700

    Fixes Civil Beat. Fixes #205

--- newshomepages/sources/javascript/civilbeat.js
+++ newshomepages/sources/javascript/civilbeat.js
@@ -1,7 +1,12 @@
-document.querySelectorAll(
-  '.poston-campaign'
-).forEach(el => el.style.display = 'none')
+var styles = `
+.poston-campaign, #om-h8bkg1lyokdvbncwkfgg, .Campaign, .CampaignType--popup, .Campaign--css {
+  display: none !important;
+}
+`;
+var styleSheet = document.createElement("style")
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
 
 document.querySelectorAll(
-  '.poston-campaign'
+  '.poston-campaign,#om-h8bkg1lyokdvbncwkfgg,.Campaign,.CampaignType--popup,.Campaign--css'
 ).forEach(el => el.remove())
