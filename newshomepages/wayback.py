18312d208c45cf1821f161e32250991039100f75
palewire <b@palewi.re>
2022-08-04 14:19:58 -0700

    Refactor wayback command to get rid of the bundle and to use requests instead of curl. Added some extra crawling options as well.

--- newshomepages/wayback.py
+++ newshomepages/wayback.py
@@ -1,9 +1,7 @@
 import os
-import time
-import typing
 
 import click
-from rich import print
+import requests
 
 from . import utils
 
@@ -11,48 +9,27 @@ IA_ACCESS_KEY = os.getenv("IA_ACCESS_KEY")
 IA_SECRET_KEY = os.getenv("IA_SECRET_KEY")
 
 
-@click.group()
-def cli():
-    """Shoot a screenshot."""
-    pass
-
-
-@cli.command()
+@click.command()
 @click.argument("handle")
-def single(handle: str):
+def cli(handle: str):
     """Archive a URL."""
     # Pull the source’s metadata
     site = utils.get_site(handle)
     # Upload it
-    _curl_url(site["url"])
-
-
-@cli.command()
-@click.argument("slug")
-def bundle(slug: str):
-    """Archive a bundle of sources."""
-    site_list = utils.get_sites_in_bundle(slug)
-    for site in site_list:
-        # Upload
-        _curl_url(site["url"])
-        time.sleep(2.5)
-
-
-def _curl_url(url):
-    print(f"💾 Asking archive.org to save {url} via the Save Page Now API")
-    command_list: typing.List[str] = [
-        "curl",
-        "-X",
-        "POST",
-        "-H",
-        "'Accept: application/json'",
-        "-H",
-        f"'Authorization: LOW {IA_ACCESS_KEY}:{IA_SECRET_KEY}'",
-        "-d",
-        f"'url={url}'",
+    r = requests.post(
         "https://web.archive.org/save",
-    ]
-    os.system(" ".join(command_list))
+        headers={
+            "Authorization": f"LOW {IA_ACCESS_KEY}:{IA_SECRET_KEY}",
+            "Accept": "application/json",
+        },
+        data={
+            "url": site["url"],
+            "capture_screenshot": "1",
+            "skip_first_archive": "1",
+            "capture_all": "1",
+        },
+    )
+    click.echo(r.json())
 
 
 if __name__ == "__main__":
